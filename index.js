42b25adf314b0dc3bec084c0e83c6c1b2af9f016
yongsannnn <yong_s@live.com>
2021-04-01 09:29:50 +0000

    Added dependency and tested route with hbs

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const hbs = require("hbs");
+const wax = require("wax-on");
+require("dotenv").config();
+
+// create an instance of express app
+let app = express();
+
+// set the view engine
+app.set("view engine", "hbs");
+
+// static folder
+app.use(express.static("public"));
+
+// setup wax-on
+wax.on(hbs.handlebars);
+wax.setLayoutPath("./views/layouts");
+
+// enable forms
+app.use(
+    express.urlencoded({
+        extended: false
+    })
+);
+
+// import routes
+const landingRoutes = require("./routes/landing")
+
+function main() {
+    app.use("/", landingRoutes)
+}
+
+main()
+
+app.listen(3000, () => {
+    console.log("Server has started");
+});
