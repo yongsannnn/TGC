c3d6def414364e0e1d4912a907df3339ce767a52
yongsannnn <yong_s@live.com>
2021-04-05 09:28:14 +0000

    Added flash msg and sessions

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const hbs = require("hbs");
 const wax = require("wax-on");
 require("dotenv").config();
+const session = require("express-session")
+const flash = require("connect-flash")
 
 // create an instance of express app
 let app = express();
@@ -22,6 +24,24 @@ app.use(
         extended: false
     })
 );
+
+// setup session
+app.use(session({
+    "secret": "nil",
+    "resave": false,
+    "saveUninitialized": true
+}))
+
+// setup flash
+app.use(flash())
+
+
+// setup global middleware
+app.use(function(req,res,next){
+    res.locals.success_msg = req.flash("success_msg")
+    res.locals.error_msg = req.flash("error_msg")
+    next()
+})
 
 // import routes
 const landingRoutes = require("./routes/landing")
