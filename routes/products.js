ca74f73f1f33d023891c50c514fba23e2b09d2e8
yongsannnn <yong_s@live.com>
2021-04-09 08:06:05 +0000

    Added DAL and checked CRUD for products

--- routes/products.js
+++ routes/products.js
@@ -1,31 +1,23 @@
 const express = require("express");
 const router = express.Router();
-const { bootstrapField, createProductForm, createSearchForm , bootstrapFieldCol3, bootstrapFieldCol6} = require('../forms');
+const { bootstrapField, createProductForm, createSearchForm, bootstrapFieldCol3, bootstrapFieldCol6 } = require('../forms');
 const { Tea, Brand, Origin, Type, Package, Flavour } = require("../models")
 const { checkIfAuthenticated } = require("../middleware")
- 
+const productDAL = require("../dal/products")
+
 // READ ALL
 router.get("/", async (req, res) => {
     // For Search
-    const allBrands = await Brand.fetchAll().map((b) => {
-        return [b.get("id"), b.get("name")]
-    })
-    const allOrigins = await Origin.fetchAll().map((o) => {
-        return [o.get("id"), o.get("name")]
-    })
-    const allTypes = await Type.fetchAll().map((t) => {
-        return [t.get("id"), t.get("name")]
-    })
-    const allPackages = await Package.fetchAll().map((p) => {
-        return [p.get("id"), p.get("name")]
-    })
-    const allFlavour = await Flavour.fetchAll().map((f) => {
-        return [f.get("id"), f.get("name")]
-    })
-    allBrands.unshift([0,"-"])
-    allOrigins.unshift([0,"-"])
-    allTypes.unshift([0,"-"])
-    allPackages.unshift([0,"-"])
+    const allBrands = await productDAL.getAllBrands()
+    const allOrigins = await productDAL.getAllOrigins()
+    const allTypes = await productDAL.getAllTypes()
+    const allPackages = await productDAL.getAllPackages()
+    const allFlavour = await productDAL.getAllFlavours()
+
+    allBrands.unshift([0, "-"])
+    allOrigins.unshift([0, "-"])
+    allTypes.unshift([0, "-"])
+    allPackages.unshift([0, "-"])
     const searchForm = createSearchForm(allBrands, allOrigins, allTypes, allPackages, allFlavour)
 
     // query connector
@@ -43,7 +35,7 @@ router.get("/", async (req, res) => {
                 "form": form.toHTML(bootstrapField)
             })
         },
-        "error": async(form)=>{
+        "error": async (form) => {
             let teas = await q.fetch({
                 withRelated: ["brand", "origin", "type", "package", "flavour"]
             })
@@ -54,31 +46,29 @@ router.get("/", async (req, res) => {
                 "form": form.toHTML(bootstrapField)
             })
         },
-        "success": async(form)=>{
+        "success": async (form) => {
             if (form.data.name) {
                 q = q.where("name", "like", "%" + form.data.name + "%")
             }
-            if (form.data.brand_id !== "0"){
+            if (form.data.brand_id !== "0") {
                 q = q.where("brand_id", "=", form.data.brand_id)
             }
-            if (form.data.origin_id !== "0"){
+            if (form.data.origin_id !== "0") {
                 q = q.where("origin_id", "=", form.data.origin_id)
             }
-            if (form.data.type_id !== "0"){
+            if (form.data.type_id !== "0") {
                 q = q.where("type_id", "=", form.data.type_id)
             }
-            if (form.data.package_id !== "0"){
+            if (form.data.package_id !== "0") {
                 q = q.where("package_id", "=", form.data.package_id)
             }
-            if (form.data.flavour){
-                // console.log(form.data.flavour.split(','));
+            if (form.data.flavour) {
                 q = q.query("join", "flavours_teas", "teas.id", "tea_id").where("flavour_id", "in", form.data.flavour.split(","))
             }
             let teas = await q.fetch({
                 withRelated: ["brand", "origin", "type", "package", "flavour"]
             })
             let teasJSON = teas.toJSON()
-            // console.log(teasJSON)
             let reversedTeas = [...teasJSON].reverse()
             res.render("products/index", {
                 "products": reversedTeas,
@@ -86,38 +76,17 @@ router.get("/", async (req, res) => {
             })
         }
     })
-
-
-    // After Search
-    // let teas = await Tea.collection().fetch({
-    //     withRelated: ["brand", "origin", "type", "package", "flavour"]
-    // });
-    // let teasJSON = teas.toJSON()
-    // let reversedTeas = [...teasJSON].reverse()
-    // res.render("products/index", {
-    //     "products": reversedTeas
-    // })
 })
 
 
 // CREATE
 // GET
 router.get("/create", checkIfAuthenticated, async (req, res) => {
-    const allBrands = await Brand.fetchAll().map((b) => {
-        return [b.get("id"), b.get("name")]
-    })
-    const allOrigins = await Origin.fetchAll().map((o) => {
-        return [o.get("id"), o.get("name")]
-    })
-    const allTypes = await Type.fetchAll().map((t) => {
-        return [t.get("id"), t.get("name")]
-    })
-    const allPackages = await Package.fetchAll().map((p) => {
-        return [p.get("id"), p.get("name")]
-    })
-    const allFlavour = await Flavour.fetchAll().map((f) => {
-        return [f.get("id"), f.get("name")]
-    })
+    const allBrands = await productDAL.getAllBrands()
+    const allOrigins = await productDAL.getAllOrigins()
+    const allTypes = await productDAL.getAllTypes()
+    const allPackages = await productDAL.getAllPackages()
+    const allFlavour = await productDAL.getAllFlavours()
     const productForm = createProductForm(allBrands, allOrigins, allTypes, allPackages, allFlavour);
     res.render("products/create", {
         "form": productForm.toHTML(bootstrapFieldCol3),
@@ -126,7 +95,12 @@ router.get("/create", checkIfAuthenticated, async (req, res) => {
 
 // POST
 router.post("/create", checkIfAuthenticated, async (req, res) => {
-    const productForm = createProductForm();
+    const allBrands = await productDAL.getAllBrands()
+    const allOrigins = await productDAL.getAllOrigins()
+    const allTypes = await productDAL.getAllTypes()
+    const allPackages = await productDAL.getAllPackages()
+    const allFlavour = await productDAL.getAllFlavours()
+    const productForm = createProductForm(allBrands, allOrigins, allTypes, allPackages, allFlavour);
     productForm.handle(req, {
         "success": async (form) => {
             let { flavour, ...productData } = form.data
@@ -150,29 +124,17 @@ router.post("/create", checkIfAuthenticated, async (req, res) => {
 // UPDATE
 // GET 
 router.get("/:product_id/update", checkIfAuthenticated, async (req, res) => {
-    const allBrands = await Brand.fetchAll().map((b) => {
-        return [b.get("id"), b.get("name")]
-    })
-    const allOrigins = await Origin.fetchAll().map((o) => {
-        return [o.get("id"), o.get("name")]
-    })
-    const allTypes = await Type.fetchAll().map((t) => {
-        return [t.get("id"), t.get("name")]
-    })
-    const allPackages = await Package.fetchAll().map((p) => {
-        return [p.get("id"), p.get("name")]
-    })
-    const allFlavour = await Flavour.fetchAll().map((f) => {
-        return [f.get("id"), f.get("name")]
-    })
-    const product = await Tea.where({
-        "id": req.params.product_id
-    }).fetch({
-        require: true,
-        withRelated: ["flavour"]
-    })
+    const allBrands = await productDAL.getAllBrands()
+    const allOrigins = await productDAL.getAllOrigins()
+    const allTypes = await productDAL.getAllTypes()
+    const allPackages = await productDAL.getAllPackages()
+    const allFlavour = await productDAL.getAllFlavours()
+
+    const product = await productDAL.getTeaById(req.params.product_id)
     const productJSON = product.toJSON()
+
     const selectedFlavourId = productJSON.flavour.map(f => f.id)
+
     const form = createProductForm(allBrands, allOrigins, allTypes, allPackages, allFlavour);
     form.fields.name.value = product.get("name")
     form.fields.cost.value = product.get("cost")
@@ -198,15 +160,15 @@ router.get("/:product_id/update", checkIfAuthenticated, async (req, res) => {
 
 // POST 
 router.post("/:product_id/update", checkIfAuthenticated, async (req, res) => {
-    const product = await Tea.where({
-        "id": req.params.product_id
-    }).fetch({
-        require: true,
-        withRelated: ["flavour"]
-    })
+    const product = await productDAL.getTeaById(req.params.product_id)
+    const allBrands = await productDAL.getAllBrands()
+    const allOrigins = await productDAL.getAllOrigins()
+    const allTypes = await productDAL.getAllTypes()
+    const allPackages = await productDAL.getAllPackages()
+    const allFlavour = await productDAL.getAllFlavours()
+    const productForm = createProductForm(allBrands, allOrigins, allTypes, allPackages, allFlavour);
     const productJSON = product.toJSON()
     const selectedFlavourId = productJSON.flavour.map(f => f.id)
-    const productForm = createProductForm();
     productForm.handle(req, {
         "success": async (form) => {
             let { flavour, ...productData } = form.data
@@ -229,11 +191,7 @@ router.post("/:product_id/update", checkIfAuthenticated, async (req, res) => {
 // DELETE
 // GET
 router.get("/:product_id/delete", checkIfAuthenticated, async (req, res) => {
-    const product = await Tea.where({
-        "id": req.params.product_id
-    }).fetch({
-        require: true
-    })
+    const product = await productDAL.getTeaById(req.params.product_id)
     res.render("products/delete", {
         "product": product.toJSON()
     })
@@ -241,11 +199,7 @@ router.get("/:product_id/delete", checkIfAuthenticated, async (req, res) => {
 
 // POST
 router.post("/:product_id/delete", checkIfAuthenticated, async (req, res) => {
-    const product = await Tea.where({
-        "id": req.params.product_id
-    }).fetch({
-        require: true
-    })
+    const product = await productDAL.getTeaById(req.params.product_id)
     await product.destroy()
     req.flash("success_msg", "Selected tea has been deleted.")
     res.redirect("/products")
