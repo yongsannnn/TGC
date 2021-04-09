43e510be2a5292e90f781c021d044378898d89a8
yongsannnn <yong_s@live.com>
2021-04-09 08:06:05 +0000

    Added DAL and checked CRUD for products

---
+++ dal/products.js
@@ -0,0 +1,53 @@
+const { Tea, Brand, Origin, Type, Package, Flavour } = require("../models")
+
+const getAllTeas = async () => {
+    return await Tea.fetchAll()
+}
+
+const getAllBrands = async () => {
+    const allBrands = await Brand.fetchAll().map((b) => {
+        return [b.get("id"), b.get("name")]
+    })
+    return allBrands
+}
+
+const getAllOrigins = async () => {
+    const allOrigins = await Origin.fetchAll().map((o) => {
+        return [o.get("id"), o.get("name")]
+    })
+    return allOrigins
+}
+
+const getAllTypes = async () => {
+    const allTypes = await Type.fetchAll().map((t) => {
+        return [t.get("id"), t.get("name")]
+    })
+    return allTypes
+}
+
+const getAllPackages = async () => {
+    const allPackages = await Package.fetchAll().map((p) => {
+        return [p.get("id"), p.get("name")]
+    })
+    return allPackages
+}
+
+const getAllFlavours = async () => {
+    const allFlavour = await Flavour.fetchAll().map((f) => {
+        return [f.get("id"), f.get("name")]
+    })
+    return allFlavour
+}
+const getTeaById = async (id) => {
+    const tea = await Tea.where({
+        "id": id
+    }).fetch({
+        require: true,
+        withRelated: ["flavour"]
+    })
+    return tea
+}
+
+module.exports = {
+    getAllTeas, getAllBrands, getAllOrigins, getAllTypes, getAllPackages, getAllFlavours, getTeaById,
+}
