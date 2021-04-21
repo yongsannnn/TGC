5db9d5af7f74b0c5176a42de6f259cbe8d7aed18
yongsannnn <yong_s@live.com>
2021-04-21 09:31:26 +0000

    Added route to get individual order details

--- routes/api/order.js
+++ routes/api/order.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const { Order, Purchase } = require("../../models")
 
 
-router.get("/:user_id", async (req, res) => {
+router.get("/all/:user_id", async (req, res) => {
     let userId = req.params.user_id
     let orders = await Order.where({
         "user_id": userId
@@ -24,9 +24,24 @@ router.get("/ind/:order_id", async (req, res) => {
         "order_id": orderId
     }).fetchAll({
         require: false,
-        withRelated: ["tea", "tea.brand", "tea.origin", "tea.type", "tea.package", "tea.flavour", "order"]
+        withRelated: ["tea", "tea.brand", "tea.origin", "tea.type", "tea.package", "tea.flavour"]
     })
     res.send(purchases)
-    
+
+})
+
+router.get("/:order_id", async (req, res) => {
+    let orderId = req.params.order_id
+    let orders = await Order.where({
+        "id": orderId
+    }).fetch({
+        require: false,
+        withRelated: ["status"]
+    })
+    if (orders) {
+        res.send(orders)
+    } else {
+        res.send("No Orders")
+    }
 })
 module.exports = router
