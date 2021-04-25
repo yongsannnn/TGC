4acd99f496dee9116669cab1a9bef9965825d7b7
yongsannnn <yong_s@live.com>
2021-04-25 15:54:56 +0000

    added search query for package and type id for API route

--- routes/api/products.js
+++ routes/api/products.js
@@ -1,22 +1,29 @@
 const express = require("express")
 const router = express.Router();
 const productDAL = require("../../dal/products")
-const {Tea} = require("../../models")
+const { Tea } = require("../../models")
 
 router.get("/", async (req, res) => {
     const allTeas = await productDAL.getAllTeas()
     res.send(allTeas)
 })
 
-router.get("/:tea_id", async ( req,res)=> {
+router.get("/:tea_id", async (req, res) => {
     const selectedTea = await productDAL.getTeaById(req.params.tea_id)
     res.send(selectedTea)
 })
 
-router.post("/search", async(req,res)=>{
-    let name = req.body.name
+router.post("/search", async (req, res) => {
     let q = Tea.collection();
-    q = q.where("name", "like","%" + name + "%")
+    if (req.body.name) {
+        q = q.where("name", "like", "%" + req.body.name + "%")
+    }
+    if (req.body.package_id) {
+        q = q.where("package_id", "=", req.body.package_id)
+    }
+    if (req.body.type_id) {
+        q = q.where("type_id", "=", req.body.type_id)
+    }
     const searchTea = await q.fetch({
         require: false,
         withRelated: ["flavour", "brand", "package", "origin", "type"]
