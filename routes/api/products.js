8d680f10c87a8f18c70de637758321f04a03d281
yongsannnn <yong_s@live.com>
2021-04-25 15:19:56 +0000

    added search query for product in api route

--- routes/api/products.js
+++ routes/api/products.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const router = express.Router();
 const productDAL = require("../../dal/products")
-
+const {Tea} = require("../../models")
 
 router.get("/", async (req, res) => {
     const allTeas = await productDAL.getAllTeas()
@@ -11,6 +11,17 @@ router.get("/", async (req, res) => {
 router.get("/:tea_id", async ( req,res)=> {
     const selectedTea = await productDAL.getTeaById(req.params.tea_id)
     res.send(selectedTea)
+})
+
+router.post("/search", async(req,res)=>{
+    let name = req.body.name
+    let q = Tea.collection();
+    q = q.where("name", "like","%" + name + "%")
+    const searchTea = await q.fetch({
+        require: false,
+        withRelated: ["flavour", "brand", "package", "origin", "type"]
+    })
+    res.send(searchTea)
 })
 
 module.exports = router
